d3d368c7c499d30e23d30b3afeec548ffaa6b747
changhoonYoo <kuilcake@gmail.com>
2023-04-17 23:04:57 +0900

    리뷰DB설계

--- WebContent/views/product/review.sql
+++ WebContent/views/product/review.sql
@@ -1,11 +1,12 @@
 create table review_p(
-	p_no number(38) not null
-	,m_id varchar2(50) not null
-	,p_img1 varchar2(150) not null
-	,p_name varchar2(50) not null
-	,re_no number(38) primary key
-	,re_cont varchar2(4000) not null
-	,re_date date
+	p_no number(38) not null		--리뷰 상품 번호
+	,m_id varchar2(50) not null		--아이디
+	,p_img1 varchar2(150) not null	--리뷰 상품 이미지
+	,p_name varchar2(50) not null	--리뷰 상품 이름
+	,re_no number(38) primary key	--리뷰 번호
+	,re_cont varchar2(4000) not null--리뷰 내용
+	,re_star number(38) not null	--별점
+	,re_date date					--등록 날짜
 );
 
 create sequence re_no_seq
@@ -16,3 +17,5 @@ nocache; --임시 메모리 사용하지 않겠다는 뜻
 ---------------------------------------------------
 
 select * from review_p;
+
+drop table review_p;
